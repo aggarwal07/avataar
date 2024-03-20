84627c68dc0426c4be72035240ce62a4f01f0e64
Aggarwal0007 <shivam@foxena.com>
2024-03-21 03:21:07 +0530

    Lato Font added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='flex'>
-      
+      <div className='logo'>
+        <img src="" alt="Logo" />
+        <p className=''>E-COMM</p>
+      </div>
     </div>
   )
 }
